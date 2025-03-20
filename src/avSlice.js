073f332424e1347313ec60d7a09ceb5334a89243
sbcybersolutions <christian@sbcyber.net>
2025-03-20 07:03:17 -0400

    completed project

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,52 +4,49 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-    img: "./projector.jpg",
-    name: "Projectors",
-    cost: 200,
-    quantity: 0,
+      img: "/projector.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
     },
     {
-        img:"./speakers.jpg",
-        name: "Speaker",
-        cost: 35,
-        quantity: 0,
+      img: "/speakers.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
     },
     {
-        img: "./microphone.jpg",
-        name: "Microphones",
-        cost: 45,
-        quantity: 0,
+      img: "/microphone.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
     },
     {
-        img: "./whiteboard.jpg",
-        name: "Whiteboards",
-        cost: 80,
-        quantity: 0,
+      img: "/whiteboard.png",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
     },
     {
-        img: "./signs.jpg",
-        name: "Signage",
-        cost: 80,
-        quantity: 0,
-    }, 
+      img: "/signs.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
   ],
-
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item) {
-            item.quantity++;
-        }
-      
+      const item = state.find((item, index) => index === action.payload);
+      if (item) {
+        item.quantity += 1;
+      }
     },
     decrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item && item.quantity > 0){
-            item.quantity--;
-        }
-     
+      const item = state.find((item, index) => index === action.payload);
+      if (item && item.quantity > 0) {
+        item.quantity -= 1;
+      }
     },
   },
 });
